640bac77997601b0b72b5f741ae466588e6cd611
jhougomont <joshuabenjaminhill@gmail.com>
2022-10-26 14:38:49 -0400

    update secrets

--- quickstart.py
+++ quickstart.py
@@ -3,7 +3,7 @@ import pygsheets
 import pandas as pd
 
 # Authorization
-gc = pygsheets.authorize(service_file='creds.json')
+gc = pygsheets.authorize(service_file='.streamlit/secrets.toml')
 
 # Open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
 sh = gc.open('food')
